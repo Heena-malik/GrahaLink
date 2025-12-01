ffc79818dffbf461eaa8d362811e0c8e716ffa03
Gagan-Khattra <gagankhattra1997@gmail.com>
2025-12-01 16:09:01 +0530

    Some Updatitions

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 
-
 import Home from './pages/Home';
 import FreeKundli from './pages/FreeKundli';
 import KundliCompatibility from './pages/KundliCompatibility';
 import SignIn from './pages/sign-In';
-import Register from './pages/Register'
+import Register from './pages/Register';
+
 function App() {
   return (
     <Router>
@@ -20,8 +20,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/FreeKundli" element={<FreeKundli />} />
           <Route path="/KundliCompatibility" element={<KundliCompatibility />} />
-           <Route path="/signin" element={<SignIn />} /> 
-            <Route path="/register" element={<Register />} /> 
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </main>
 
